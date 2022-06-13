1d948c527765e0946723a3e77eda9305289b4cf2
eltunas170 <beto.martinez.velazquez@gmail.com>
2022-06-13 12:36:11 -0700

    project upload

---
+++ MinimalAPIPostgresSQLEF/Data/OfficeDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPostgresSQLEF.Models;
+
+namespace MinimalAPIPostgresSQLEF.Data
+{
+    public class OfficeDb : DbContext
+    {
+        public OfficeDb(DbContextOptions<OfficeDb> options) : base(options) { }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
